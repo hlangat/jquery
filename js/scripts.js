17ec993cf8eb586b89c274d1eb1d0f983906219b
hlangat <hlangat@gmail.com>
2018-07-04 21:25:30 +0300

    add update to css file

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,19 @@ $(document).ready(function() {
     alert("This is an image.");
   });
 
-    $("h2").click(function() {
-      alert("This is heading 2");
-    });
+  $("h2").click(function() {
+    alert("This is heading 2");
   });
+  $("p").click(function() {
+    $("img").show();
+  });
+  $("p").click(function() {
+    $(".walrus-showing").show();
+    $(".walrus-hidden").hide();
+  });
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+
+});
