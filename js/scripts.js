b10de60cfdaa602e86462e1397caea31f2842a14
hlangat <hlangat@gmail.com>
2018-07-05 13:01:12 +0300

    update css,index and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,16 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
   });
 
-  $("img").click(function() {
-    alert("This is an image.");
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
-
-  $("h2").click(function() {
-    alert("This is heading 2");
-  });
-  $("p").click(function() {
-    $("img").show();
-  });
-  $("p").click(function() {
-    $(".walrus-showing").show();
-    $(".walrus-hidden").hide();
-  });
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
-  });
-
 });
