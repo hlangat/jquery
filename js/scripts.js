a70f2b831decf771b212162227a8d9beaa64a9e7
hlangat <hlangat@gmail.com>
2018-07-05 14:32:05 +0300

    updated js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
-        $("button#green").click(function() {
-          $("body").addClass("green-background");
-        });
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
 
-        $("button#yellow").click(function() {
-          $("body").addClass("yellow-background");
-        });
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
 
-        $("button#red").click(function() {
-          $("body").addClass("red-background");
-        });
-        $("button#green").click(function() {
-          $("body").removeClass();
-          $("body").addClass("green-background");
-        });
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
 
-        $("button#yellow").click(function() {
-          $("body").removeClass();
-          $("body").addClass("yellow-background");
-        });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
 
-        $("button#red").click(function() {
-          $("body").removeClass();
-          $("body").addClass("red-background");
-        });
-      });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#green").click(function() {
+    $("body").css("background-color", "green");
+  });
+  $("button#yellow").click(function() {
+    $("body").css("background-color", "yellow");
+  });
+  $("button#red").click(function() {
+    $("body").css("background-color", "red");
+  });
+
+});
