012061c51558164bfa7c1adbf38b2543baa10ca6
hlangat <hlangat@gmail.com>
2018-07-05 14:09:42 +0300

    updated index css and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
-  });
+        $("button#green").click(function() {
+          $("body").addClass("green-background");
+        });
 
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
+        $("button#yellow").click(function() {
+          $("body").addClass("yellow-background");
+        });
 
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-});
+        $("button#red").click(function() {
+          $("body").addClass("red-background");
+        });
+        $("button#green").click(function() {
+          $("body").removeClass();
+          $("body").addClass("green-background");
+        });
+
+        $("button#yellow").click(function() {
+          $("body").removeClass();
+          $("body").addClass("yellow-background");
+        });
+
+        $("button#red").click(function() {
+          $("body").removeClass();
+          $("body").addClass("red-background");
+        });
+      });
